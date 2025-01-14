43f428fef1644602d5d31e70f039285d7fe03128
Erik <eekihenriksson@gmail.com>
2025-01-14 09:40:54 +0200

    Update run.py

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ prompts = [
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
-llm = LLM(model="meta-llama/Llama-3.3-70B-Instruct")
+llm = LLM(model="meta-llama/Llama-3.3-70B-Instruct", tensor_parallel_size=4)
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
